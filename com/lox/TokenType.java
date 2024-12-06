e934e8a45904b43e3e3e8b7b51d42957ef036e11
Ayush <hackerboy5556@gmail.com>
2024-12-06 19:30:06 +0530

    Add TokenTypes for the lox language

---
+++ com/lox/TokenType.java
@@ -0,0 +1,28 @@
+package com.lox;
+
+enum TokenType {
+    // Single Character tokesn
+    LEFT_PAREN, RIGHT_PAREN, 
+    LEFT_BRACE, RIGHT_BRACE, 
+    LEFT_BRACKET, RIGHT_BRACKET, 
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, 
+    SLASH, STAR,
+
+
+    // One or two character tokens
+    BANG, BANG_EQUAL, 
+    EQUAL, EQUAL_EQAUL, 
+    LESS, LESS_EQUAL, 
+    GREATER, GREATER_EQUAL, 
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER, 
+
+    // Keywords
+    IF, ELSE, OR, AND, CLASS, 
+    THIS, SUPER, FUN, TRUE, FALSE, 
+    VAR, FOR, WHILE, NIL, PRINT, 
+    RETURN,
+
+    EOF
+}
