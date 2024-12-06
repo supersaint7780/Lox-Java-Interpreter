23be80f91c7c9a357c3a2751f17805be5208adbf
Ayush <hackerboy5556@gmail.com>
2024-12-06 19:26:03 +0530

    Create the interpreter initial point

---
+++ com/lox/Lox.java
@@ -0,0 +1,72 @@
+package com.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lox {
+
+    private static boolean hadError = false;
+    private static final int IncorrectCommandUsage = 64;
+    private static final int IncorrectInputData = 65;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(IncorrectCommandUsage);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Path.of(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) {
+            System.exit(IncorrectInputData);
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        // REPL (read eval print loop)
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    private static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[Line " + line + " ] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
