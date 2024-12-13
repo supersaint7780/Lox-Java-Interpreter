7baac52b2834cd324214a5e365a6ead269795e5f
Ayush <hackerboy5556@gmail.com>
2024-12-14 00:46:54 +0530

    refactor

--- com/lox/Lox.java
+++ com/lox/Lox.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+// import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.util.List;
-import com.lox.Scanner;
 
 public class Lox {
 
